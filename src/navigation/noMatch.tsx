a6ea16e90d6365a3819a894f87103cdf38219634
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-09-16 11:34:03 -0400

    make no match page fit site theme

--- src/navigation/noMatch.tsx
+++ src/navigation/noMatch.tsx
@@ -1,9 +1,13 @@
+import './noMatch.css';
+
 /**
  * Component for when no match can be found for a url
  * @param props properties for this component
  */
 export default function NoMatch(props: any) {
     return <>
-        Page Not Found
+        <div className='no-match-div'>
+            Page Not Found
+        </div>
     </>;
 }
