49c8662a6a78e79d0534dafc8f829e1adbc19786
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-09-16 23:23:17 -0400

    add cursor to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { setMaxListeners } from 'events';
 import { useEffect, useRef, useState } from 'react';
 import Vivus from 'vivus';
 import logoTrace from '../../public/images/logo-trace.svg';
@@ -9,14 +10,42 @@ import './home.css';
 const animationDuration = 5500;
 
 /**
+ * Time between text changes in milliseconds
+ */
+const textInterval = 5000;
+
+/**
+ * Time between backspaces in milliseconds
+ */
+const backspaceInterval = 75;
+
+/**
+ * Array of possible text options
+ */
+const textOptions: string[] = ["Aspiring Full Stack Developer", "University Of Toronto", "Computer Science"];
+
+/**
+ * @param prevItem the previously selected item
+ * @returns a random item from the text options
+ */
+function randomItem(prevItem: string | undefined = undefined) {
+    let activeTextOptions = prevItem ? textOptions.filter(x => x != prevItem) : textOptions;
+    return activeTextOptions[Math.floor(Math.random() * activeTextOptions.length)];
+}
+
+/**
  * Component for the home page
  * @param props properties for this component
  */
 export default function Home(props: any) {
 
+    const [adding, setAdding] = useState(false);
+
     const ref = useRef<HTMLObjectElement>(null);
 
-    const [text, setText] = useState("Full Stack Developer");
+    const [selectedText, setSelectedText] = useState(randomItem());
+
+    const [text, setText] = useState(selectedText);
 
     useEffect(() => {
 
@@ -36,6 +65,24 @@ export default function Home(props: any) {
         });
     }, []);
 
+    useEffect(() => {
+        if (adding) {
+            if (text == selectedText) {
+                setTimeout(() => setAdding(false), textInterval);
+            } else {
+                setTimeout(() => setText(prev => selectedText.substring(0, prev.length + 1)), backspaceInterval);
+            }
+        } else {
+            if (text.length === 0) {
+                setTimeout(() => {
+                    setSelectedText(randomItem());
+                    setAdding(true);
+                }, backspaceInterval);
+            } else {
+                setTimeout(() => setText(prev => prev.substring(0, prev.length - 1)), backspaceInterval);
+            }
+        }
+    }, [text, selectedText, adding]);
     return <>
         <div className="landing-screen">
             <object className="landing-logo" ref={ref} type="image/svg+xml" data={logoTrace} />
